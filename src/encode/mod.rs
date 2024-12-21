d33905d3a59a34c7882c671a556665c8ff69c6b3
funnsam <funnsam8@gmail.com>
2024-12-21 18:38:52 +0800

    encoding

--- src/encode/mod.rs
+++ src/encode/mod.rs
@@ -2,6 +2,40 @@ mod alphanumeric_table;
 
 use crate::*;
 
+fn encode(string: &str, buffer: &mut BitsWriter, mode: Mode, version: Version, ec: ErrorCorrectLv) -> Option<()> {
+    buffer.write_bits(4, mode.indicator() as usize);
+    buffer.write_bits(version.char_count_length(mode), string.len());
+
+    match mode {
+        Mode::Numeric => encode_numeric(string, buffer),
+        Mode::Alphanumeric => encode_alphanumeric(string, buffer),
+        Mode::Bytes => encode_bytes(string, buffer),
+        Mode::Kanji => todo!(),
+    }?;
+
+    let bytes = version.max_data_bytes(ec);
+
+    buffer.write_bits((bytes * 8 - buffer.len()).min(4), 0);
+    buffer.align();
+
+    let mut even = true;
+    for _ in buffer.bits.len()..=bytes {
+        buffer.write_u8_aligned(if even { 0b11101100 } else { 0b00010001 });
+        even ^= true;
+    }
+
+    Some(())
+}
+
+#[test]
+fn test_encode() {
+    let mut w = BitsWriter::new();
+    assert!(encode("HELLO WORLD", &mut w, Mode::Alphanumeric, Version::new(1), ErrorCorrectLv::Q).is_some());
+
+    assert_eq!(&w.bits, &[0b00100000, 0b01011011, 0b00001011, 0b01111000, 0b11010001, 0b01110010, 0b11011100, 0b01001101, 0b01000011, 0b01000000, 0b11101100, 0b00010001, 0b11101100, 0]);
+    assert_eq!(w.last_length, 0);
+}
+
 fn encode_numeric(string: &str, buffer: &mut BitsWriter) -> Option<()> {
     let mut iter = string.chars().array_chunks::<3>();
     for p in &mut iter {
